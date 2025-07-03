73a0b2a5cc0bd8d6d8ad7a9b365c48e986002552
Abhin A <abhin.a@Abhins-MacBook-Air.local>
2025-07-03 16:37:58 +0530

    provider

--- provider.tf
+++ provider.tf
@@ -1,15 +1,8 @@
-provider "google" {
-  credentials = file("../key.json")
-
-  project = var.project_id
-  region  = var.region
-  zone    = var.zone
-}
 terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.51.0"
+      version = ">= 5.0.0"
     }
   }
 }
