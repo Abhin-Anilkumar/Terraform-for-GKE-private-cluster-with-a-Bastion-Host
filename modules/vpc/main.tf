0c652f16cf16cbe13ead440e44b7279b704d6d48
Abhin A <abhin.a@Abhins-MacBook-Air.local>
2025-07-03 16:58:33 +0530

    vpc1

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -35,8 +35,8 @@ resource "google_compute_router" "task_router" {
     #nat_ip_allocate_option             = "AUTO_ONLY"
     #source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
 
-  subnetwork {
-    name                    = google_compute_subnetwork.subnets[0].id
-    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
-  }
-}
+ # subnetwork {
+  #  name                    = google_compute_subnetwork.subnets[0].id
+ ##   source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+#  }
+#}
